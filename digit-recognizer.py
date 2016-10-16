13eee041358a565fb6d1835747b811dbdb46bba3
Diego Pereira <rockin.jack@gmail.com>
2016-10-16 15:12:42 -0200

    feat: improved model, almost 100% train accuracy

--- digit-recognizer.py
+++ digit-recognizer.py
@@ -28,27 +28,44 @@ encoded_labels = encoder.transform(labels)
 categorical_labels = np_utils.to_categorical(encoded_labels)
 
 
-print("-> Splitting train set...")
-train_labels, train_pixels = categorical_labels[:33600], pixels[:33600]
-print("-> Splitting test set...")
-test_labels, test_pixels = categorical_labels[33600:], pixels[33600:]
-
+train_pixels = pixels
+train_labels = categorical_labels
 
 model = Sequential()
-model.add(Dense(32, input_dim=784, init='uniform', activation='relu'))
-model.add(Dense(16, init='uniform', activation='relu'))
+model.add(Dense(128, input_dim=784, init='uniform', activation='relu'))
+model.add(Dense(64, init='uniform', activation='relu'))
 model.add(Dense(10, init='uniform', activation='sigmoid'))
 
 print("-> Model created")
 # Compile model
-model.compile(loss='binary_crossentropy', optimizer='adam', metrics=['accuracy'])
+model.compile(loss='binary_crossentropy', optimizer='adamax', metrics=['accuracy'])
 print("-> Model compiled")
 # Fit the model
-model.fit(train_pixels, train_labels, nb_epoch=150)
+model.fit(train_pixels, train_labels, nb_epoch=50)
 print("-> Model fit")
 # evaluate the model
 scores = model.evaluate(train_pixels, train_labels)
 print("%s: %.2f%%" % (model.metrics_names[1], scores[1]*100))
-scores = model.evaluate(test_pixels, test_labels)
-print("%s: %.2f%%" % (model.metrics_names[1], scores[1]*100))
-get_ipython().magic('logstart experiment1.py')
+
+testset = numpy.loadtxt("data/test.csv", delimiter=",")
+testset
+testset.shape
+submission = model.predict(testset)
+submission
+submission.shape
+dataset
+pixels
+type(submission)
+submission[0].argmax()
+val = lambda x: x.argmax()
+result = [val(x) for x in submission]
+result
+result[:10]
+submission[:10]
+
+with open("./rnn_submission5.csv", "w+") as f:
+    f.write("ImageId,Label\n")
+    count = 0
+    for r in result:
+        count += 1
+        f.write("%s,%s\n" % (count, r))
